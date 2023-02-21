5e7925fbfa24b5554f4b21a3b323dafaaf27c4b6
yathsh <yathishkhgowda@gmail.com>
2023-02-21 14:31:37 +0530

    first commit

---
+++ springwebapp-2/src/org/tnsif/springwebapp/packageSpringwebapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tnsif.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springwebapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
